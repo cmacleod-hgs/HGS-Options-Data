1a616ec15d48e4227b9a4752afeea96918b957df
Colin MacLeod <gw15macleodcolin@glow.sch.uk>
2025-12-18 14:20:29 +0000

    Add core configuration files

---
+++ app.py
@@ -0,0 +1,11 @@
+"""
+Main Flask application entry point
+"""
+from flask import Flask
+from config import Config
+from app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
